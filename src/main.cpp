5846eeb310cba4f9875acb888b41fcddcffe2f53
JavaLich <javalich@outlook.com>
2020-05-05 15:12:47 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿// Tracer.cpp : Defines the entry point for the application.
+//
+
+#include "Tracer.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. This is a test" << endl;
+	cin.get();
+	return 0;
+}
